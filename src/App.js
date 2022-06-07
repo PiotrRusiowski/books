3db273c4c93171790b9fedab96ff1c0e37feee3f
unknown <piorus9196@gmail.com>
2022-06-07 17:38:02 +0200

    add axios with url adress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        getTopReatedMove();
+    }, []);
+    const getTopReatedMove = () => {
+        axios
+            .get("http://localhost:3001/api/books/"
+            )
+            .then((res) => {
+                console.log(res)
+            })
+            .catch((err) => console.log(err))
+    };
+    return (
+        <div className="App">
+            <h1>jestem</h1>
+        </div>
+    );
 }
 
 export default App;
